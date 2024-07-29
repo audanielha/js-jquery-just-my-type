af54a9d5fcb49670af47a6fc02d3bf9f06e6b991
Daniel Ha <audanielha@gmail.com>
2024-07-29 22:41:53 +1200

    add base and complete 2 tasks

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+// Hides Uppercase Keyboard on Load
+$('#keyboard-upper-container').css('display', 'none');
+
+// While the shift key is held down, hide the lowercase keyboard and show the uppercase one
+$(document).on('keydown', function(e){
+    if(e.keyCode===16){
+        $('#keyboard-upper-container').css('display', 'block');
+        $('#keyboard-lower-container').css('display', 'none');
+    }
+})
+// When the shift key is released, show the lowercase keyboard and hide the uppercase one
+$(document).on('keyup', function(e){
+    if(e.keyCode===16){
+        $('#keyboard-upper-container').css('display', 'none');
+        $('#keyboard-lower-container').css('display', 'block');
+    }
+})
+// When keys are pressed, they should be highlighted in the browser.
+$(document).on('keydown', function(e){
+    console.log(e.keyCode)
+    $(`#${e.keyCode}`).css('background-color','green');
+
+})
+
+});
