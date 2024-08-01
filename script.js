7abbfa9114f4255b8392e9eea36557292821fd8d
Daniel Ha <audanielha@gmail.com>
2024-08-01 23:35:33 +1200

    highlight the currently expected letter in the on-screen sentence that should be typed nex

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).on('keydown', function(e){
         $('#keyboard-upper-container').css('display', 'block');
         $('#keyboard-lower-container').css('display', 'none');
     }
-})
+});
 // When the shift key is released, show the lowercase keyboard and hide the uppercase one
 $(document).on('keyup', function(e){
     if(e.keyCode===16){
@@ -21,6 +21,21 @@ $(document).on('keyup', function(e){
 $(document).on('keydown', function(e){
         $(`#${e.keyCode}`).css('background-color','green');
         $(`#${e.keyCode+32}`).css('background-color','green');
-})
-
 });
+$(document).on('keyup', function(e){
+    $(`#${e.keyCode}`).css('background-color','unset');
+    $(`#${e.keyCode+32}`).css('background-color','unset');
+});
+
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
+let counter = 0; 
+$('#sentence').append(sentences[counter])
+});
+let spacing = 1;
+$(document).on('keydown', function(){
+
+    $('#yellow-block').css('margin-left', `${15 * spacing}px`);
+    spacing++
+});
+
